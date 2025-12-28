b040bd4a9607dfb2e14d0c6fd88ea848e84b1235
Onuora Promise Ebube <Promaxonuora18@gmail.com>
2025-12-28 18:10:36 +0000

    FINAL_STABILITY: Destructured Show model import in index.js

--- db/index.js
+++ db/index.js
@@ -1,15 +1,21 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-const Show = require('./showModel');
+
+// THE FIX: You MUST use curly braces {} here because showModel exports an object
+const { Show } = require('./showModel'); 
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("Vault Uplink Established (MongoDB)"))
     .catch(err => console.error("Vault Connection Error:", err));
 
 const getWatchlist = async () => {
-    return await Show.find({});
+    return await Show.find({}); // Now this will work!
 };
+
 const saveWatchlist = async (showData) => {
     await Show.findOneAndUpdate({ id: showData.id }, showData, { upsert: true });
 };
+
 module.exports = { Show, getWatchlist, saveWatchlist };
+
+
