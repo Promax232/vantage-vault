d6d8fb30bd90d6839e98eba9d7ff598e643a885a
Onuora Promise Ebube <Promaxonuora18@gmail.com>
2025-12-28 17:42:16 +0000

    Refactor: separated routes, fixed db exports, added express.urlencoded

--- db/showModel.js
+++ db/showModel.js
@@ -1,22 +1,32 @@
 const mongoose = require('mongoose');
 
 const showSchema = new mongoose.Schema({
-    id: String,
-    title: String,
-    poster: String,
-    type: String,
-    source: String,
-    currentEpisode: Number,
-    totalEpisodes: Number,
-    personalRating: Number,
-    status: { type: String, default: 'watching' },
-    logs: { type: Map, of: Object, default: {} },
-    startDate: String
+  id: String,
+  title: String,
+  poster: String,
+  type: String,
+  source: String,
+  currentEpisode: Number,
+  totalEpisodes: Number,
+  personalRating: Number,
+  status: { type: String, default: 'watching' },
+  logs: { type: Map, of: Object, default: {} },
+  startDate: String
 });
 
 const Show = mongoose.model('Show', showSchema);
 
-const getWatchlist = async () => await Show.find({});
-const saveWatchlist = async (showData) => await Show.findOneAndUpdate({ id: showData.id }, showData, { upsert: true });
+// Helper functions
+const getWatchlist = async () => {
+  return await Show.find({});  // THIS REQUIRES Show to be a Mongoose model
+};
 
-module.exports = { Show, getWatchlist, saveWatchlist };
+const saveWatchlist = async (showData) => {
+  await Show.findOneAndUpdate({ id: showData.id }, showData, { upsert: true });
+};
+
+module.exports = {
+  Show,
+  getWatchlist,
+  saveWatchlist
+};
