e2975d31eb5cc6efccb52cd1c2cb164e0e291e18
Onuora Promise Ebube <Promaxonuora18@gmail.com>
2026-01-03 15:49:39 +0000

    Final Calibration: Corrected relative paths for nested API routes

--- routes/api/watchlist.js
+++ routes/api/watchlist.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { Show } = require('../db');
+const { Show, getWatchlist, saveWatchlist } = require('../../db');
 
 
 router.get('/save', async (req, res) => {
